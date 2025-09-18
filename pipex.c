7ad44f62ca5b1d4df2e65d3d79e8c61cadd7ebb1
Clément N <nicolas-clement1997@hotmail.fr>
2025-09-18 14:19:02 +0200

    Almost there

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 10:27:13 by clnicola          #+#    #+#             */
-/*   Updated: 2025/09/18 13:02:05 by clnicola         ###   ########.fr       */
+/*   Updated: 2025/09/18 14:16:55 by clnicola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,28 @@
 
 void	exec(char *cmd, char **env)
 {
-	execve(cmd)
+	char *path;
+	char **args;
+
+	args = ft_split(cmd, ' ');
+	path = get_cmd(env, args[0]);
+	if(!path)
+	{
+		ft_printf("command not found: %s\n",args[0]);
+		exit(0);
+	}
+	execve(path, args, env);
+}
+
+int open_file(char *name, int fd)
+{
+	int id;
+
+	if(fd == 0)
+		id = open(name, O_RDONLY);
+	if(fd == 1)
+		id = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	return(id);
 }
 
 void child(char **av, int *p_fd, char **env)
@@ -25,7 +46,7 @@ void child(char **av, int *p_fd, char **env)
 	dup2(fd, 0);
 	dup2(p_fd[1], 1);
 	close(p_fd[0]);
-	//exec();
+	exec(av[2], env);
 }
 
 void parent(char **av, int *p_fd, char **env)
@@ -36,7 +57,7 @@ void parent(char **av, int *p_fd, char **env)
 	dup2(fd, 1);
 	dup2(p_fd[0], 0);
 	close(p_fd[1]);
-	//exec();
+	exec(av[3], env);
 }
 
 int main(int ac, char **av, char **env)
--- pipex_utils.c
+++ pipex_utils.c
@@ -6,17 +6,42 @@
 /*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:30:36 by clnicola          #+#    #+#             */
-/*   Updated: 2025/09/18 12:20:35 by clnicola         ###   ########.fr       */
+/*   Updated: 2025/09/18 14:06:57 by clnicola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int check_args(int ac)
-{	
-	if(ac != 5)
+char *get_path(char **env)
+{
+	int i = 0;
+
+	while(env[i])
 	{
-		ft_printf("Arg error\n");
+		if(ft_strncmp(env[i], "PATH=", 5) == 0)
+			return(env[i] + 5);
+		i++;
 	}
-	return(0);
+	return(NULL);
+}
+
+char	*get_cmd(char **env, char *cmd)
+{
+	int i;
+	char **path;
+	char *temp;
+	char *full_path;
+
+	i = 0;
+	path = ft_split(get_path(env), ':');
+	while(path[i])
+	{
+		temp = ft_strjoin(path[i],"/");
+		full_path = ft_strjoin(temp, cmd);
+		if(access(full_path, X_OK) == 0)
+			return(full_path);
+		free(full_path);
+		i++;
+	}
+	return(NULL);
 }
